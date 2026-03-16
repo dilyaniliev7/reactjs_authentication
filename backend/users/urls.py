1a772704b7becf5810e99053c7d835738445f91d
dilyaniliev7 <dilyaniliev7@abv.bg>
2026-03-16 20:05:48 +0200

    adding the token to the API requests

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -6,5 +6,6 @@ from .views import *
 router = DefaultRouter()
 router.register('register', RegisterViewSet, basename='register')
 router.register('login', LoginViewSet, basename='login')
+router.register('users', UserViewSet, basename='users')
 
 urlpatterns = router.urls
