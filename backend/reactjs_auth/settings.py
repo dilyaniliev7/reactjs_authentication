1400f4117d872a533f4992b84c7508c2ae6c8120
dilyaniliev7 <dilyaniliev7@abv.bg>
2026-03-17 23:03:48 +0200

    email template added

--- backend/reactjs_auth/settings.py
+++ backend/reactjs_auth/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'reactjs_auth.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
