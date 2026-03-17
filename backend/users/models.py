d07e3692cb01cedd0c8969b4d680f33f79498c1d
dilyaniliev7 <dilyaniliev7@abv.bg>
2026-03-17 23:14:00 +0200

    sending an email, sitelinks added

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.base_user import BaseUserManager
+from django_rest_passwordreset.signals import reset_password_token_created
+from django.dispatch import receiver
+from django.urls import reverse
+from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
+from django.utils.html import strip_tags
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
@@ -31,3 +37,14 @@ class CustomUser(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+@receiver(reset_password_token_created)
+def password_reset_token_created(reset_password_token, *args, **kwargs):
+    sitelink = "http://localhost:5173/"
+    token = "?token={}".format(reset_password_token.key)
+    full_link = str(sitelink)+str("password-reset")+str(token)
+
+    context = {
+        'full_link': full_link,
+        'email_adress': reset_password_token.user.email
+    }
